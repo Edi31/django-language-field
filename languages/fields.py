4aed569902985d846858bfc84960fd07baa03553
Edi 31 <stdrequest@gmail.com>
2020-05-02 18:10:27 +0200

    add a 2 size management of language field

--- languages/fields.py
+++ languages/fields.py
@@ -1,4 +1,6 @@
 from django.db.models.fields import CharField
+import importlib
+import os
 
 class LanguageField(CharField):
     """
@@ -6,8 +8,15 @@ class LanguageField(CharField):
     """
     def __init__(self, *args, **kwargs):
         # Local import so the languages aren't loaded unless they are needed.
-        from .languages import LANGUAGES
+        #from .languages import LANGUAGES
+        if 'size' in kwargs:
+            size_option='_'+kwargs['size']
+        else:
+            size_option=''
+        languages = importlib.import_module('languages.languages' + size_option , os.path.dirname(__file__))
+        LANGUAGES = languages.LANGUAGES
 
-        kwargs.setdefault('max_length', 3)
+        kwargs.setdefault('max_length', 4)
         kwargs.setdefault('choices', LANGUAGES)
+        kwargs.pop('size', "")
         super(CharField, self).__init__(*args, **kwargs)
